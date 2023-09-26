00ddd82c5f146f481bd71c7a22abdd4341bfebdf
Steve De Jongh <de.jongh.steve@gmail.com>
2023-09-26 18:05:55 -0400

    Practice problems - functions

--- Practice_Problems/functions.js
+++ Practice_Problems/functions.js
@@ -77,3 +77,95 @@ console.log(myVar);
 // Notice that on line 2 there is no variable declaration for myVar (i.e., there is no var keyword before myVar). As a result of this, JavaScript looks in the outer scope for the declaration. Since it doesn't exist, JavaScript binds myVar to be a "property" of the global object. This is "almost" the same as if myVar was globally declared. We will discuss more about why this is "almost"—but not "exactly"—the same in a later course when we cover the global / window object.
 
 // 6
+
+let a = 7;
+
+function myValue(b) {
+  b += 10;
+}
+
+myValue(a);
+console.log(a);
+
+// The code logs 7 to the console. The value assigned to variable `a` is 7 and it is a primitive value. Primitive values in JavaScript are immutable. Therefore, passing the variable `a` that is assigned a primitive value as an argument to a function, and consequently using it on any operation in the body of the function does not have any effect on the value assigned to `a`.
+
+// 7
+
+let a = 7;
+
+function myValue(a) {
+  a += 10;
+}
+
+myValue(a);
+console.log(a);
+
+// This will log 7
+
+// This code is very similar to the example above, and although the functions parameter is also given the name `a`, this does not change the outcome. In this example now, the concept shown is variable shadowing. 
+
+//The main difference is that this code demonstrates the concept of variable shadowing. In JavaScript, when an argument is passed to a function, a local variable with the same name as the corresponding parameter is created within the inner scope of the function. Therefore, there are two variables named a that exist in this program: one in the global scope and the other in the function's local scope. Therefore, the reassignment of the local variable a within the function has no effect on the global variable a.
+
+
+// 8
+
+// To do later...
+
+// 9
+
+console.log(a);
+
+var a = 1;
+
+// This will log `undefined`.
+
+// This is an example of `hoisting`. On `line 113` the program initializes `a` to the value of 1. Breaking this down further, first the variable `a` is declared, then it is assigned the value of 1.
+
+// The JavaScript interpreter doesn't immediately execute all of a programs code line by line, instead it first goes over the code to find and associate variable delcaratiomns with their appropriate scope. Visually, this is as if Javascript moves each variable declaration to the top of tits scope, but the assignment operation is not hoisted.
+
+// This result in the code looking like:
+
+var a;
+console.log(a);
+
+a = 1;
+
+// As such, the variable `a` is declared at the time it's passed to `console.log`, however it's value is undefined.
+
+// 10
+
+logValue();
+
+function logValue() {
+  console.log('Hello, world!');
+}
+
+// This code will output `Hello, word!` as a result of hoisting.
+
+// With function declarations, everything is hoisted including the name and body. This code is equivalent to:
+
+function logValue() {
+  console.log('Hello, world!');
+}
+
+logValue();
+
+// FE:
+
+var logValue = 'foo';
+
+function logValue() {
+  console.log('Hello, world!');
+}
+
+console.log(typeof logValue);
+
+// After hoisting is equivalent to:
+
+function logValue() {
+  console.log('Hello, world!');
+}
+
+logValue = 'foo';
+
+console.log(typeof logValue);
