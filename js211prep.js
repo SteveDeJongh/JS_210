e87c728868be96479817213f6b46394fa01d409a
Steve De Jongh <de.jongh.steve@gmail.com>
2023-11-21 14:22:44 -0800

    JS 211 prep

--- js211prep.js
+++ js211prep.js
@@ -46,3 +46,88 @@ function sortAndPrintLetters(obj) {
 
 sortAndPrintLetters({a:1, k:3, g:2}) // => 'aggkkk'
 
+
+function countWords(string) {
+  let words = string.split(' ');
+
+  return words.length;
+}
+
+countWords('hello world'); // => 2
+
+function countIndividualWords(string) {
+  let words = string.toLowerCase().split(/[^a-z]/);
+  let answer = {};
+
+  for (let i = 0; i < words.length; i += 1) {
+    let word = words[i];
+    if (word.length <= 0) {
+      continue;
+    }
+
+    if (Object.keys(answer).includes(word)) {
+      answer[word] += 1;
+    } else {
+      answer[word] = 1;
+    }
+  }
+
+  return answer;
+}
+
+countIndividualWords('Hello, hello world'); // => {'hello' : 2, 'world' : 1};
+
+function countSubstr(input, match) {
+  let count = 0;
+
+  for (let i = 0; i < input.length; i += 1) {
+    if (input.indexOf(match, i) > 0) {
+      count += 1;
+      i = input.indexOf(match, i) + 1;
+    }
+  }
+  return count;
+}
+
+countSubstr('Hello, Hello! How are you doing Mellody?', 'ello' );// => 3
+
+function countEle(arr, match) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === match) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+countEle(['name', 'year', 'age', 'name'], 'name'); // => 2
+countEle([1,2,3,4,5,1,2,3,4], 2); //=> 2
+
+function sorting(arr) {
+  return arr.sort((a,b) => b - a);
+}
+
+sorting([3,5,1,2,11,456]); //=> [456,11,5,3,2,1];
+
+
+function isAlphabetical(string) {
+  let chars = string.split('');
+  
+  for (let i = 0; i < chars.length - 1; i += 1) {
+    if (chars[i] < chars[i + 1]) {
+      console.log(`${chars[i]} vs ${chars[i + 1]}`)
+      return false;
+    }
+  }
+  return true
+}
+isAlphabetical('abcdefg'); //true
+isAlphabetical('adegttksn'); //false
+
+function extractStr(strings) {
+  return strings.split(' ').filter((str) => str[0] === 'a');
+}
+
+extractStr('abcd bcd aa'); //[abcd, aa]
